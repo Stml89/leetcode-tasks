ffd8e01b126c98d6a6f866635db00700a845908b
Siarhei Stamal <sergeistamal@gmail.com>
2025-07-01 05:08:59 +0200

    Fix solution #680

--- easy/680.Valid-Palindrome-II.py
+++ easy/680.Valid-Palindrome-II.py
@@ -19,19 +19,35 @@ Constraints:
 1 <= s.length <= 105
 s consists of lowercase English letters.
 """
-# TODO
 def validPalindrome(s: str) -> bool:
-    if s == s[::-1]:
-        return True
-    curr = 0
-    while curr < len(s) - 1:
-        tmp = s[:curr] + s[curr + 1:]
-        if tmp == tmp[::-1]:
-            return True
-        curr += 1
-    return False
+    # O(n)
+    start = 0
+    end = len(s) - 1
+
+    while start < end:
+        if s[start] != s[end]:
+            skip_start = s[start + 1: end + 1]
+            skip_end = s[start:end]
+            return skip_start == skip_start[::-1] or skip_end == skip_end[::-1]
+        start += 1
+        end -= 1
+
+    return True
+
+    # O(n2)
+    # if s == s[::-1]:
+    #     return True
+    # curr = 0
+    # while curr < len(s):
+    #     tmp = s[:curr] + s[curr + 1:]
+    #     print(tmp)
+    #     if tmp == tmp[::-1]:
+    #         return True
+    #     curr += 1
+    # return False
 
 
 assert validPalindrome("aba")
 assert validPalindrome("abca")
 assert not validPalindrome("abc")
+assert validPalindrome("eccer")
