dd6e51b2a5f05af7d8cbf3d0ab388171e8c9f768
Siarhei Stamal <sergeistamal@gmail.com>
2025-10-13 08:36:42 +0200

    Add another solution for task #925

--- easy/925.Long-Pressed-Name.py
+++ easy/925.Long-Pressed-Name.py
@@ -31,7 +31,25 @@ def isLongPressedName(name: str, typed: str) -> bool:
             cnt += 1
         elif cnt == 0 or typed[i] != name[cnt - 1]:
             return False
+
     return cnt == len(name)
+
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+def isLongPressedName1(name: str, typed: str) -> bool:
+    i, j = 0, 0
+
+    while j < len(typed):
+        if i < len(name) and name[i] == typed[j]:
+            i += 1
+            j += 1
+        elif j > 0 and typed[j] == typed[j - 1]:
+            j += 1
+        else:
+            return False
+
+    return i == len(name)
 
 
 assert isLongPressedName(name="alex", typed="aaleex")
