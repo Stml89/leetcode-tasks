cb9ed62771d95693ad023dde2e0c2659732b410d
Siarhei Stamal <sergeistamal@gmail.com>
2025-12-17 05:12:47 +0100

    Add another solution for task #1114

--- easy/1114.Print-in-Order.py
+++ easy/1114.Print-in-Order.py
@@ -53,3 +53,24 @@ class Foo:
     def third(self, printThird: Callable[[], None]) -> None:
         self.secondDone.acquire()
         printThird()
+
+
+class Foo1:
+    def __init__(self):
+        self.second_lock = Lock()
+        self.third_lock = Lock()
+        self.second_lock.acquire()
+        self.third_lock.acquire()
+
+    def first(self, printFirst: Callable[[], None]) -> None:
+        printFirst()
+        self.second_lock.release()
+
+    def second(self, printSecond: Callable[[], None]) -> None:
+        self.second_lock.acquire()
+        printSecond()
+        self.third_lock.release()
+
+    def third(self, printThird: Callable[[], None]) -> None:
+        self.third_lock.acquire()
+        printThird()
