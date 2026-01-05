21bbc33f824a23c7ac5860d372b024504475e218
Siarhei Stamal <sergeistamal@gmail.com>
2026-01-05 12:26:48 +0100

    Fix issue in task #1185

--- easy/1185.Day-of-the-Week.py
+++ easy/1185.Day-of-the-Week.py
@@ -33,26 +33,29 @@ from datetime import datetime
 
 # Time complexity: O(year - 1971 + month)
 # Space complexity: O(1)
-def dayOfTheWeek1(day: int, month: int, year: int) -> str:
+def dayOfTheWeek(day: int, month: int, year: int) -> str:
+    def isLeapYear(year: int) -> bool:
+        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
     days_of_week = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"]
-    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_in_month = [31, 29 if isLeapYear(year) else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     total_days = 0
 
     for y in range(1971, year):
-        total_days += 366 if (y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)) else 365
+        total_days += 366 if isLeapYear(y) else 365
 
-    for m in range(1, month):
-        total_days += days_in_month[m - 1]
-        if m > 2 and (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
-            total_days += 1
+    for i in range(month - 1):
+        total_days += days_in_month[i]
 
     total_days += day - 1
 
     return days_of_week[total_days % 7]
 
 
-def dayOfTheWeek(day: int, month: int, year: int) -> str:
+# Time complexity: O(1)
+# Space complexity: O(1)
+def dayOfTheWeek1(day: int, month: int, year: int) -> str:
     date_obj = datetime(year, month, day)
     weekday_name = date_obj.strftime('%A')
 
