913fe534093b91ba26bf94a2a705f0273b830b92
Siarhei Stamal <sergeistamal@gmail.com>
2026-02-09 06:41:13 +0100

    Add solution for task #1304

---
+++ easy/1304.Find-N-Unique-Integers-Sum-up-to-Zero.py
@@ -0,0 +1,46 @@
+"""
+1304. Find N Unique Integers Sum up to Zero
+
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+Example 2:
+Input: n = 3
+Output: [-1,0,1]
+
+Example 3:
+Input: n = 1
+Output: [0]
+
+Constraints:
+1 <= n <= 1000
+
+Hint 1
+Return an array where the values are symmetric. (+x , -x).
+
+Hint 2
+If n is odd, append value 0 in your returned array.
+"""
+from typing import List
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+def sumZero(n: int) -> List[int]:
+    result = []
+    for i in range(1, n // 2 + 1):
+        result.append(-i)
+        result.append(i)
+    if n % 2 == 1:
+        result.append(0)
+
+    return result
+
+
+assert sumZero(5) == [-1, 1, -2, 2, 0]
+assert sumZero(3) == [-1, 1, 0]
+assert sumZero(1) == [0]
