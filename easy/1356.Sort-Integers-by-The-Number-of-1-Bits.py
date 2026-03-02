24b9fb89eafdcd5f85db468f1970a613e7c181a0
Siarhei Stamal <sergeistamal@gmail.com>
2026-03-02 07:54:45 +0100

    Add another solution for task #1356

--- easy/1356.Sort-Integers-by-The-Number-of-1-Bits.py
+++ easy/1356.Sort-Integers-by-The-Number-of-1-Bits.py
@@ -41,5 +41,11 @@ def sortByBits(arr: List[int]) -> List[int]:
     return [x for _, x in sorted(zip(result, arr))]
 
 
+# Time complexity: O(n * log M + n log n)
+# Space complexity: O(n)
+def sortByBits1(arr: List[int]) -> List[int]:
+    return sorted(arr, key=lambda x: (bin(x).count('1'), x))
+
+
 assert sortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8]) == [0, 1, 2, 4, 8, 3, 5, 6, 7]
 assert sortByBits([1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]) == [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
