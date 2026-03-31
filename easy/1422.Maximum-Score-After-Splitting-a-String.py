53f81f6de1eebcefa1d0b05b1606cc7c3f795e27
Siarhei Stamal <sergeistamal@gmail.com>
2026-03-31 05:39:58 +0200

    Add another solution for task #1422

--- easy/1422.Maximum-Score-After-Splitting-a-String.py
+++ easy/1422.Maximum-Score-After-Splitting-a-String.py
@@ -66,6 +66,25 @@ def maxScore1(s: str) -> int:
     return ans
 
 
+# Time complexity: O(n)
+# Space complexity: O(n)
+def maxScore2(s: str) -> int:
+    left_zeros = 0
+    right_ones = s.count("1")
+
+    max_score = 0
+
+    for i in range(len(s) - 1):
+        if s[i] == "0":
+            left_zeros += 1
+        else:
+            right_ones -= 1
+
+        max_score = max(max_score, left_zeros + right_ones)
+
+    return max_score
+
+
 assert maxScore("00111") == 5
 assert maxScore("011101") == 5
 assert maxScore("1111") == 3
