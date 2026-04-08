122245fe7ac39b5cab94cfe344441cee14a900d0
Siarhei Stamal <sergeistamal@gmail.com>
2026-04-08 07:17:36 +0200

    Add another solution for task #1450

--- easy/1450.Number of-Students-Doing-Homework-at-a-Given-Time.py
+++ easy/1450.Number of-Students-Doing-Homework-at-a-Given-Time.py
@@ -46,5 +46,17 @@ def busyStudent(startTime: List[int], endTime: List[int], queryTime: int) -> int
     return count
 
 
+# Time complexity: O(n)
+# Space complexity: O(1)
+def busyStudent1(startTime: List[int], endTime: List[int], queryTime: int) -> int:
+    count = 0
+
+    for i in range(len(startTime)):
+        if startTime[i] <= queryTime <= endTime[i]:
+            count += 1
+
+    return count
+
+
 assert busyStudent([1, 2, 3], [3, 2, 7], 4) == 1
 assert busyStudent([4], [4], 4) == 1
