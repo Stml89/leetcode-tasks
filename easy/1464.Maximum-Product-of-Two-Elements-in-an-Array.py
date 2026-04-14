ac56491bc1bdb5fb766d98733e7550a0d4feaa1b
Siarhei Stamal <sergeistamal@gmail.com>
2026-04-14 11:20:40 +0200

    Add another solution for task #1464

--- easy/1464.Maximum-Product-of-Two-Elements-in-an-Array.py
+++ easy/1464.Maximum-Product-of-Two-Elements-in-an-Array.py
@@ -42,6 +42,13 @@ def maxProduct(nums: List[int]) -> int:
     return (max1 - 1) * (max2 - 1)
 
 
+# Time complexity: O(n log n)
+# Space complexity: O(n)
+def maxProduct1(nums: List[int]) -> int:
+    nums.sort()
+    return (nums[-1] - 1) * (nums[-2] - 1)
+
+
 assert maxProduct([3, 4, 5, 2]) == 12
 assert maxProduct([1, 5, 4, 5]) == 16
 assert maxProduct([3, 7]) == 12
