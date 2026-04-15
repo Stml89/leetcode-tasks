2af9f03de47da5c2d582af6551d39301473fa527
Siarhei Stamal <sergeistamal@gmail.com>
2026-04-15 08:25:53 +0200

    Add another solution for task #1470

--- easy/1470.Shuffle-the-Array.py
+++ easy/1470.Shuffle-the-Array.py
@@ -53,6 +53,15 @@ def shuffle1(nums: List[int], n: int) -> List[int]:
     return result
 
 
+# Time complexity: O(n)
+# Space complexity: O(n)
+def shuffle2(nums: List[int], n: int) -> List[int]:
+    s = []
+    for i in zip(nums[:n], nums[n:]):
+        s.extend(i)
+    return s
+
+
 assert shuffle([2, 5, 1, 3, 4, 7], 3) == [2, 3, 5, 4, 1, 7]
 assert shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4) == [1, 4, 2, 3, 3, 2, 4, 1]
 assert shuffle([1, 1, 2, 2], 2) == [1, 2, 1, 2]
