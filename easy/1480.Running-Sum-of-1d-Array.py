51340ac4dbefa454ce7bcb2a66d85aee8cb95ae0
Siarhei Stamal <sergeistamal@gmail.com>
2026-04-20 06:50:13 +0200

    Add another solution for task #1480

--- easy/1480.Running-Sum-of-1d-Array.py
+++ easy/1480.Running-Sum-of-1d-Array.py
@@ -38,6 +38,19 @@ def runningSum(nums: List[int]) -> List[int]:
     return result
 
 
+# Time complexity: O(n)
+# Space complexity: O(n)
+def runningSum1(nums: List[int]) -> List[int]:
+    result = []
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(nums[i])
+        else:
+            result.append(result[i - 1] + nums[i])
+
+    return result
+
+
 assert runningSum([1, 2, 3, 4]) == [1, 3, 6, 10]
 assert runningSum([1, 1, 1, 1, 1]) == [1, 2, 3, 4, 5]
 assert runningSum([3, 1, 2, 10, 1]) == [3, 4, 6, 16, 17]
