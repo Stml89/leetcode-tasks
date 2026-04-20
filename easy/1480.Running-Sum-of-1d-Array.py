126dc1027a3397174939b09f83205241e29a9fe7
Siarhei Stamal <sergeistamal@gmail.com>
2026-04-20 06:50:03 +0200

    Add solution for task #1480

---
+++ easy/1480.Running-Sum-of-1d-Array.py
@@ -0,0 +1,43 @@
+"""
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+
+Hint 1
+Think about how we can calculate the i-th number in the running sum from the (i-1)-th number.
+"""
+from typing import List
+
+
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+def runningSum(nums: List[int]) -> List[int]:
+    result = []
+    for i in range(len(nums)):
+        result.append(sum(nums[:i + 1]))
+
+    return result
+
+
+assert runningSum([1, 2, 3, 4]) == [1, 3, 6, 10]
+assert runningSum([1, 1, 1, 1, 1]) == [1, 2, 3, 4, 5]
+assert runningSum([3, 1, 2, 10, 1]) == [3, 4, 6, 16, 17]
