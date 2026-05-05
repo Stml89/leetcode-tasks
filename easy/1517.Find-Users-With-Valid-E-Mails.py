e7ac76effd13b5443019b2c28cbc6eca632fc85b
Siarhei Stamal <sergeistamal@gmail.com>
2026-05-05 08:16:44 +0200

    Fix solution for task #1517

--- easy/1517.Find-Users-With-Valid-E-Mails.py
+++ easy/1517.Find-Users-With-Valid-E-Mails.py
@@ -51,4 +51,4 @@ The mail of user 7 starts with a period.
 """
 # SELECT user_id, name, mail
 # FROM Users
-# WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9\_\.\-]*@leetcode.com')
+# WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9\_\.\-]*@leetcode\\.com$')
