2e9d5660da1e3e6aa0034fe8112a03adb5296c83
Siarhei Stamal <sergeistamal@gmail.com>
2026-07-07 08:08:10 +0200

    Add solution for task #1656

---
+++ easy/1656.Design-an-Ordered-Stream.py
@@ -0,0 +1,81 @@
+"""
+1656. Design an Ordered Stream
+
+There is a stream of n (idKey, value) pairs arriving in an arbitrary order, where idKey is an integer between
+1 and n and value is a string. No two pairs have the same id.
+Design a stream that returns the values in increasing order of their IDs by returning a chunk (list) of values after
+each insertion. The concatenation of all the chunks should result in a list of the sorted values.
+
+Implement the OrderedStream class:
+- OrderedStream(int n) Constructs the stream to take n values.
+- String[] insert(int idKey, String value) Inserts the pair (idKey, value) into the stream, then returns the largest possible chunk of currently inserted values that appear next in the order.
+
+Example:
+Input
+["OrderedStream", "insert", "insert", "insert", "insert", "insert"]
+[[5], [3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]]
+Output
+[null, [], ["aaaaa"], ["bbbbb", "ccccc"], [], ["ddddd", "eeeee"]]
+
+Explanation
+// Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
+OrderedStream os = new OrderedStream(5);
+os.insert(3, "ccccc"); // Inserts (3, "ccccc"), returns [].
+os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
+os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
+os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
+os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+// Concatentating all the chunks returned:
+// [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
+// The resulting order is the same as the order above.
+
+Constraints:
+1 <= n <= 1000
+1 <= id <= n
+value.length == 5
+value consists only of lowercase letters.
+Each call to insert will have a unique id.
+Exactly n calls will be made to insert.
+
+Hint 1
+Maintain the next id that should be outputted.
+
+Hint 2
+Maintain the ids that were inserted in the stream.
+
+Hint 3
+Per each insert, make a loop where you check if the id that has the turn has been inserted, and if so increment
+the id that has the turn and continue the loop, else break.
+"""
+from typing import List
+
+
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream = [None] * n
+        self.ptr = 0
+
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey - 1] = value
+        res = []
+        while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+            res.append(self.stream[self.ptr])
+            self.ptr += 1
+        return res
+
+
+# Your OrderedStream object will be instantiated and called as such:
+obj = OrderedStream(5)
+param_1 = obj.insert(3, "ccccc")
+assert param_1 == []
+param_1 = obj.insert(1, "aaaaa")
+assert param_1 == ["aaaaa"]
+param_1 = obj.insert(2, "bbbbb")
+assert param_1 == ["bbbbb", "ccccc"]
+param_1 = obj.insert(5, "eeeee")
+assert param_1 == []
+param_1 = obj.insert(4, "ddddd")
+assert param_1 == ["ddddd", "eeeee"]
