dac6650f2c5ad63e4b17bcd602b0741e0f62cbe9
Siarhei Stamal <sergeistamal@gmail.com>
2026-07-16 07:06:09 +0200

    Add solution for task #1678

---
+++ easy/1678.Goal-Parser-Interpretation.py
@@ -0,0 +1,57 @@
+"""
+1678. Goal Parser Interpretation
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()"
+and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o",
+and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+Given the string command, return the Goal Parser's interpretation of command.
+
+Example 1:
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+
+Example 2:
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+
+Example 3:
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+
+Constraints:
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order.
+
+Hint 1
+You need to check at most 2 characters to determine which character comes next.
+"""
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+def interpret(command: str) -> str:
+    result = []
+    i = 0
+    while i < len(command):
+        if command[i] == 'G':
+            result.append('G')
+            i += 1
+        elif command[i] == '(' and command[i + 1] == ')':
+            result.append('o')
+            i += 2
+        else:
+            result.append("al")
+            i += 4
+
+
+    return ''.join(result)
+
+
+assert interpret("G()(al)") == "Goal"
+assert interpret("G()()()()(al)") == "Gooooal"
+assert interpret("(al)G(al)()()G") == "alGalooG"
